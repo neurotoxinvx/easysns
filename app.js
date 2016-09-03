38ee1b0bf0e2259ab536c6bfe634b2bd89abbd81
NeurotoxinVX <d4:77:62:39:12:4b:7e:3e:ae:b3:70:df:e6:bb:83:d4>
2016-09-03 19:45:40 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 var http = require('http')
+var parseUrl = require('url').parse
+
+function homeController(req, res){
+  res.end('home')
+}
+
+function userController(req, res){
+  res.end('user')
+}
+
+function notFoundController(req, res) {
+  res.writeHead(404)
+  res.end('Not Found')
+}
+
+const rules = [
+  { path: '/', controller: homeController },
+  { path: '/user', controller: userController }
+]
+
+function find(ary, match) {
+	for(var i = 0; i < ary.length; i++) {
+		if(match(ary[i])) return ary[i]
+	}
+}
 
 var server = http.createServer(function(req, res){
-  res.end('hello world\n')
+  var urlInfo = parseUrl(req.url)
+
+  var rule = find(rules, function(rule){
+  	return rule.path == urlInfo.pathname
+  })
+  var controller = rule && rule.controller(req, res) || notFoundController
+  controller(req, res)
 })
 
 server.listen(3000)
