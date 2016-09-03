72c81178d58f1b8990ce7533592bd53a20ca5d91
NeurotoxinVX <d4:77:62:39:12:4b:7e:3e:ae:b3:70:df:e6:bb:83:d4>
2016-09-03 20:02:53 +0800

    add port

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 var http = require('http')
 var parseUrl = require('url').parse
+var port = process.env.PORT || 3000;
 
-function homeController(req, res){
+/*Controller*/
+function homeController (req, res) {
   res.end('home')
 }
 
-function userController(req, res){
+function userController (req, res) {
   res.end('user')
 }
 
-function notFoundController(req, res) {
+function notFoundController (req, res) {
   res.writeHead(404)
   res.end('Not Found')
 }
 
+/*router rules*/
 const rules = [
   { path: '/', controller: homeController },
   { path: '/user', controller: userController }
 ]
 
-function find(ary, match) {
+/*match router rules*/
+function find (ary, match) {
 	for(var i = 0; i < ary.length; i++) {
 		if(match(ary[i])) return ary[i]
 	}
 }
 
+/*server*/
 var server = http.createServer(function(req, res){
   var urlInfo = parseUrl(req.url)
 
@@ -35,4 +40,4 @@ var server = http.createServer(function(req, res){
   controller(req, res)
 })
 
-server.listen(3000)
+server.listen(port)
