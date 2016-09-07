f8dd069bddd5fe1ce6730f6ecf44ab318dc8cfe3
NeurotoxinVX <neurotoxin@vip.qq.com>
2016-09-07 23:35:14 +0800

    update

--- controllers/static.js
+++ controllers/static.js
@@ -6,6 +6,6 @@ module.exports = function (req, res) {
   var path = req.params[1]
   path = joinPath(publicPath, path)
   fs.readFile(path, function(err, data){
-    res.end(data)
+    res.end(data);
   })
 }
